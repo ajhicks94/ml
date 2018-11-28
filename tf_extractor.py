0e35c3a441521fce036b8c2b409af78b19d3b13e
Anthony Hicks <ajhicks94@gmail.com>
2018-11-28 00:01:19 -0600

    Add load_data()

--- tf_extractor.py
+++ tf_extractor.py
@@ -4,7 +4,7 @@
 # Version: 2018-10-09
 
 # Parameters:
-# --inputDataset=<directory>
+# --x_train_dir=<directory>
 #   Directory that contains the articles XML file with the articles for which a prediction should be made.
 # --outputFile=<file>
 #   File to which the term frequency vectors will be written. Will be overwritten if it exists.
@@ -34,41 +34,41 @@ from keras.datasets import imdb
 def parse_options():
     """Parses the command line options."""
     try:
-        long_options = ["inputDataset=", "label_dir=", "outputFile=", "max_size="]
+        long_options = ["x_train_dir=", "labelDir=", "outputFile=", "max_size="]
         opts, _ = getopt.getopt(sys.argv[1:], "d:l:o:n:", long_options)
     except getopt.GetoptError as err:
         print(str(err))
         sys.exit(2)
 
-    inputDataset = "undefined"
-    label_dir = "undefined"
+    x_train_dir = "undefined"
+    labelDir = "undefined"
     outputFile = "undefined"
     max_size = sys.maxsize
 
     for opt, arg in opts:
-        if opt in ("-d", "--inputDataset"):
-            inputDataset = arg
+        if opt in ("-d", "--x_train_dir"):
+            x_train_dir = arg
         elif opt in ("-l", "--labelDir"):
-            label_dir = arg
+            labelDir = arg
         elif opt in ("-o", "--outputFile"):
             outputFile = arg
         elif opt in "-n":
             max_size = arg
         else:
             assert False, "Unknown option."
-    if inputDataset == "undefined":
-        sys.exit("Input dataset, the directory that contains the articles XML file, is undefined. Use option -d or --inputDataset.")
-    elif not os.path.exists(inputDataset):
-        sys.exit("The input dataset folder does not exist (%s)." % inputDataset)
+    if x_train_dir == "undefined":
+        sys.exit("Input dataset, the directory that contains the articles XML file, is undefined. Use option -d or --x_train_dir.")
+    elif not os.path.exists(x_train_dir):
+        sys.exit("The input dataset folder does not exist (%s)." % x_train_dir)
 
-    if label_dir == "undefined":
+    if labelDir == "undefined":
         sys.exit("Label directory, the directory that contains the articles label datafile, is undefined. Use option -l or --labelDir.")
-    elif not os.path.exists(label_dir):
-        sys.exit("The label folder does not exist (%s)." % label_dir)
+    elif not os.path.exists(labelDir):
+        sys.exit("The label folder does not exist (%s)." % labelDir)
     if outputFile == "undefined":
         sys.exit("Output file, the file to which the vectors should be written, is undefined. Use option -o or --outputFile.")
 
-    return (inputDataset, label_dir, outputFile, max_size)
+    return (x_train_dir, labelDir, outputFile, max_size)
 
 def clean_and_count(article, data):
     text = lxml.etree.tostring(article, encoding="unicode", method="text")
@@ -105,7 +105,9 @@ class HyperpartisanNewsTFExtractor(xml.sax.ContentHandler):
 
     def startElement(self, name, attrs):
         if self.counter == self.max_articles:
-            err = "\nMaximum of " + str(self.counter) + " articles has been reached."
+            err = ''
+            if self.mode == 'y':
+                err = "\nMaximum of " + str(self.counter) + " articles has been reached."
             raise Exception(err)
         if name != "articles":
             if name == "article":
@@ -141,15 +143,12 @@ class HyperpartisanNewsTFExtractor(xml.sax.ContentHandler):
                         # Remember, test will build it's own word_index too
                         # This line is causing major slowdowns in runtime
                         idx = self.word_index[word]
-                        # Let's just store the index along with the count
-                        #idx = list(self.word_index).index(word)
                         row.append(idx)
                     
-                    # Append to x array (data)
+                    # Append to sequence array
                     x.append(row)
 
                     #print(article.get("id") + " completed.")
-                    
                     self.data = x
 
                 elif self.mode == "y":
@@ -171,16 +170,16 @@ class HyperpartisanNewsTFExtractor(xml.sax.ContentHandler):
                 self.counter += 1
                 self.lxmlhandler = "undefined"
 
-def create_word_index(inputDataset, mode, max_articles=sys.maxsize):
+def create_word_index(inputDir, mode, max_articles=sys.maxsize):
     idx_file = mode + ".json"
     with open(idx_file, 'w') as f:
         data = {}
         json.dump(data,f)
         f.close()
    
-    for file in os.listdir(inputDataset):
+    for file in os.listdir(inputDir):
         if file.endswith(".xml"):
-            with open(inputDataset + "/" + file) as inputRunFile:
+            with open(inputDir + "/" + file) as inputRunFile:
                 try:
                     xml.sax.parse(inputRunFile, HyperpartisanNewsTFExtractor(mode="widx", data=data, max_articles=max_articles))
                 except Exception as e:
@@ -195,13 +194,13 @@ def create_word_index(inputDataset, mode, max_articles=sys.maxsize):
 
     # Replaces dict value with its index
     # {'blue': 56, 'brown': 28, 'red': 24} => {'blue': 0, 'brown': 1, 'red': 2}
-    # This decreases runtime DRAMATICALLY. For 100 articles, went from 25s => 0.5s
+    # This decreases runtime DRAMATICALLY.
     for w in enumerate(o):
         o[w[1]] = w[0]
 
     json.dump(o, f)
     f.close()
-    print("The word counts have been written to the output file in descending order.")
+    #print("The word counts have been written to the output file in descending order.")
 
 def get_data(directory, filetype, mode, data, max_articles, word_index={}):
     for file in os.listdir(directory):
@@ -215,45 +214,30 @@ def get_data(directory, filetype, mode, data, max_articles, word_index={}):
                                             data=data,
                                             max_articles=max_articles))
                 except Exception as e:
-                    print(e)
+                    print(e, end='')
                     break
 
+def load_data(x_train_dir, labelDir, max_articles, num_words=None, skip_top=0, maxlen=None,
+              seed=113, start_char=1, oov_char=2, index_from=3):
 
-def main(inputDataset, label_dir, outFile, max_articles):
-    start = time.time()
-    # Give a True/False if word_index is created (from cmdline)
-    create_word_index(inputDataset=inputDataset, mode="training", max_articles=max_articles)
-    end = time.time()
-    print("Creating training word index took: ", end - start)
-
-    start = time.time()
     with open('training.json', 'r') as f:
         word_index = {}
         word_index = json.load(f)
         f.close()
-    end = time.time()
-    print("Loading word_index took: ", end - start)
-    start = time.time()
 
     # Start with python lists, then convert to numpy when finished for better runtime
     x_train = []
     y_train = []
 
     # Populate x_train
-    get_data(directory=inputDataset, filetype='xml', mode="x", data=x_train, max_articles=max_articles, word_index=word_index)
-    end = time.time()
-    print("x_train took: ", end - start)
+    get_data(directory=x_train_dir, filetype='xml', mode="x", data=x_train, max_articles=max_articles, word_index=word_index)
     
     # Populate y_train
-    start = time.time()
-    get_data(directory=label_dir, filetype='xml', mode="y", data=y_train, max_articles=max_articles)
-    end = time.time()
-    print("y_train took: ", end-start)
-
+    get_data(directory=labelDir, filetype='xml', mode="y", data=y_train, max_articles=max_articles)
+    
     #print(y_train)
     # Create test_word_index
-    # create_word_index(inputDataset=testDir, mode="test", max_articles=max_articles)
-
+    # create_word_index(inputDir=testDir, mode="test", max_articles=max_articles)
 
     # Get test data => x_test, y_test
     x_test = [[0, 89, 100, 201, 3, 240, 1],[1],[1],[1],[2]]
@@ -265,20 +249,11 @@ def main(inputDataset, label_dir, outFile, max_articles):
     # Transform Data TODO: PUT IN FUNCTION
     x_train = np.array(x_train)
     y_train = np.array(y_train)
-    print("len(x_train)= ", len(x_train))
-    print("x_train.shape= ", x_train.shape)
-    print("y_train.shape= ", y_train.shape)
-
-    start = time.time()
+    #print("len(x_train)= ", len(x_train))
+    #print("x_train.shape= ", x_train.shape)
+    #print("y_train.shape= ", y_train.shape)
 
     _remove_long_seq = sequence._remove_long_seq
-    seed = 113
-    start_char = 1
-    index_from = 3
-    maxlen=None
-    num_words=None
-    oov_char=2
-    skip_top=0
 
     # Makes random numbers predictable based on (seed)
     np.random.seed(seed)
@@ -303,7 +278,7 @@ def main(inputDataset, label_dir, outFile, max_articles):
     x_test = x_test[indices]
     y_test = y_test[indices]
 
-    print("x_test.shape= ", x_test.shape)
+    #print("x_test.shape= ", x_test.shape)
 
     # Append test to train
     xs = np.concatenate([x_train, x_test])
@@ -344,9 +319,27 @@ def main(inputDataset, label_dir, outFile, max_articles):
     x_train, y_train = np.array(xs[:idx]), np.array(ys[:idx])
     x_test, y_test = np.array(xs[idx:]), np.array(ys[idx:])
 
+    return (x_train, y_train), (x_test, y_test)
+
+def main(x_train_dir, labelDir, outFile, max_articles):
+    total_time = 0
+
+    start = time.time()
+    # Create word index for the training set
+    create_word_index(inputDir=x_train_dir, mode="training", max_articles=max_articles)
     end = time.time()
-    #print("Data transformation took: ", end - start)
-    # return (x_train, y_train), (x_test, y_test)
+
+    print("Creating training word index took: ", end - start)
+    total_time = end - start
     
+    start = time.time()
+    #
+    (x_train, y_train), (x_test, y_test) = load_data(x_train_dir, labelDir, max_articles)
+    end = time.time()
+
+    print("\n\nLoading and transforming data took: ", end - start)
+    total_time += (end - start)
+    print("\nTotal elapsed time: %s for %s records" %(total_time, max_articles))
+
 if __name__ == '__main__':
     main(*parse_options())
